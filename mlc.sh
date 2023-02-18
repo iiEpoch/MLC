f43c0c16bf18e8d681a81c8877590820edd5ff66
JingYu <92147862+iiEpoch@users.noreply.github.com>
2023-02-18 13:56:12 +0800

    Update mlc.sh

--- mlc.sh
+++ mlc.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 mkdir ./report
 
-./mlc --peak_injection_bandwidth | tee ./report/mlc_peak_injection_bandwidth.txt &
+./mlc --peak_injection_bandwidth | tee ./report/mlc_peak_injection_bandwidth.txt
 
-./mlc | tee ./report/mlc.txt &
+./mlc | tee ./report/mlc.txt
 
-./mlc --bandwidth_matrix -W5 | tee ./report/mlc_bandwidth_matrix.txt &
+./mlc --bandwidth_matrix -W5 | tee ./report/mlc_bandwidth_matrix.txt
 
-./mlc --loaded_latency -W5 | tee ./report/mlc_loaded_latency.txt &
+./mlc --loaded_latency -W5 | tee ./report/mlc_loaded_latency.txt
 
 ./mlc --idle_latency –c0 –J/mnt/pmem0 | tee ./report/mlc_idle_latency_pmem.txt 
